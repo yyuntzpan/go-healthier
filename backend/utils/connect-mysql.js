ce3aea95bb889e97b9121a51fcd09e5ed744a3ae
yyuntzpan <yap0602@gmail.com>
2025-03-20 18:45:40 +0800

    fix: 修正資料庫連線資訊

--- backend/utils/connect-mysql.js
+++ backend/utils/connect-mysql.js
@@ -1,6 +1,8 @@
 import mysql from "mysql2/promise";
+import dotenv from "dotenv";
+dotenv.config();
 
-const { DB_HOST, DB_USER, DB_PASS, DB_NAME } = process.env;
+const { DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT } = process.env;
 
 console.log({ DB_HOST, DB_USER, DB_PASS, DB_NAME });
 
@@ -9,6 +11,7 @@ const db = await mysql.createPool({
   user: DB_USER,
   password: DB_PASS,
   database: DB_NAME,
+  port: DB_PORT,
   waitForConnections: true,
   connectionLimit: 5,
   queueLimit: 0,
